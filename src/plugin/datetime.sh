4b43559139b68f70a81fa335ceeae83a38dc4617
InfiniteSynthesis <shenyu.tcv@gmail.com>
2023-12-17 17:59:04 +0000

    Fix: Update time format & remove stale plugins

--- src/plugin/datetime.sh
+++ src/plugin/datetime.sh
@@ -6,7 +6,7 @@ plugin_datetime_accent_color=$(get_tmux_option "@theme_plugin_datetime_accent_co
 plugin_datetime_accent_color_icon=$(get_tmux_option "@theme_plugin_datetime_accent_color_icon" "blue0")
 
 # https://man7.org/linux/man-pages/man1/date.1.html
-plugin_datetime_format=$(get_tmux_option "@theme_plugin_datetime_format" "%c")
+plugin_datetime_format=$(get_tmux_option "@theme_plugin_datetime_format" "%Y-%m-%d %H:%M")
 
 function load_plugin() {
   echo "${plugin_datetime_format}"
